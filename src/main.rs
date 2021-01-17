2de1d0b0468dba01815bf504bbfbcf7d96e84aba
Martin Bley <martin@mb-oss.de>
2021-01-17 21:08:00 +0100

    First running version

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,54 @@
 
 #[macro_use] extern crate rocket;
 
+use std::borrow::Cow;
+use regex::Regex;
+use serde::Serialize;
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
+use rocket::request::Form;
+use rocket::response::NamedFile;
+
+fn clarify(s: &str) -> Cow<str> {
+    let re = Regex::new(r"(\[|\]|BLOCKED)").unwrap();
+    re.replace_all(s, "")
+}
+
+#[derive(FromForm, Debug)]
+struct Obfuscated {
+    field_obfuscated_link: String,
+}
+
+#[post("/", data = "<clarify_form>")]
+fn sink(clarify_form: Form<Obfuscated>) -> Template {
+
+    #[derive(Serialize)]
+    struct Context {
+        clarified_link: String,
+    }
+
+    let obfuscated_link = clarify_form.into_inner().field_obfuscated_link;
+
+    let context = Context {
+        clarified_link: format!("https://{}", clarify(&obfuscated_link))
+    };
+    Template::render("home", context)
+}
+
+/*
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Option<NamedFile> {
+    NamedFile::open("static/index.html").ok()
+}
+*/
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![sink])
+        .mount("/", StaticFiles::from("static"))
+        .attach(Template::fairing())
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
-
